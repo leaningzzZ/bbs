ba02deae35cd8e145cf133394e3fc883589b334b
liuzhe <qiudaoyuandmantou@gmail.com>
2020-02-20 00:17:48 +0800

    fix proxy

--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,6 @@ module.exports = {
                 changeOrigin: true,     // target是域名的话，需要这个参数，
                 secure: false,          // 设置支持https协议的代理
             },
-            '/bpi': {
-                target: 'http://175.24.22.69:8080/html/studentManagement/',
-                pathRewrite: { '^/bpi': '' },
-                changeOrigin: true,     // target是域名的话，需要这个参数，
-                secure: false,          // 设置支持https协议的代理
-            },
         },                                           // 配置跨域处理,只有一个代理
     },
 }
